2d190fb7a0ca45a7721e239cbd6092217ba4d1ff
Trout Lake School First Robotics Team <robotics@tlschool.net>
2023-03-09 17:31:47 -0800

    recalibrated swerve modules

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,17 +31,17 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 5; //  Set front left module drive motor ID
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 3; //  Set front left module steer motor ID
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 18; //  Set front left steer encoder ID
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(306.2109375);// -Math.toRadians(117.68554687500001); //  Measure and set front left steer o
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(304.45311410219045);// -Math.toRadians(117.68554687500001); //  Measure and set front left steer o
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; //  Set front right drive ID
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4; //  Set front right steer ID
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 16; //  Set front right stcoder ID
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(247.8515625); //  Measure and set front right steer o
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(247.23631889667016); //  Measure and set front right steer o
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 8; //  Set back left drive mo
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7; //  Set back left steer mo
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 15; //  Set back left steer r ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(138.69140339931275); //  Measure and set back left steer o
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(139.13085777051137); //  Measure and set back left steer o
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6; //  Set back right drive mD
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 1; //  Set back right steer mD0
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 17; //  Set back right steeder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(354.99023437500006); //  Measure and set back right steer offset
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(354.55077581504975); //  Measure and set back right steer offset
 }
